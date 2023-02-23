837bd5425d5524da3f2c92d456684ca2492f0c30
Mhd Dedek Yansyah <dedek2203@gmail.com>
2023-02-23 16:45:09 +0700

    create theme directory and create theme.dart after that add all theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24.0;
+double defaultPadding = 24.0;
+double defaultRadius = 10.0;
+
+const Color primary = Color(0xFF5BB318);
+const Color secondary = Color(0xFF8D92A3);
+const Color lightGray = Color(0xFF8092AF);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF020202);
+const Color gray = Color(0xFFEDEEF0);
+const Color grey = Color(0xFFB1B7C2);
+
+TextStyle primaryStyle = GoogleFonts.poppins(color: primary);
+
+TextStyle secondaryStyle = GoogleFonts.poppins(color: secondary);
+TextStyle greyStyle = GoogleFonts.poppins(color: grey);
+TextStyle lightGrayStyle = GoogleFonts.poppins(color: lightGray);
+TextStyle whiteStyle = GoogleFonts.poppins(color: white);
+TextStyle blackStyle = GoogleFonts.poppins(color: black);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight blackWeight = FontWeight.w900;
